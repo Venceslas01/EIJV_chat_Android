8a66e2cdabf4d4da87972db7a3a9b69c79d3afab
GBAGUIDI Bidossessi Venceslas <venceslas@29.137.etud.wifi.u-picardie.fr>
2024-04-05 15:59:02 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EIJV_chat_Androïd"
+include(":app")
