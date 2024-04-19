41fc7a2c3c5cf4cac1b715408d037a62e4182597
venceslas <venceslasgbaguidi@yahoo.com>
2024-04-19 17:24:31 +0200

    Intégration de firebase

--- app/src/main/java/fr/upjv/eijv_chat_androd/MainActivity.java
+++ app/src/main/java/fr/upjv/eijv_chat_androd/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil_app);
+
+         FirebaseAuth mAuth;
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
     }
 }
---
+++ app/src/main/java/fr/upjv/eijv_chat_androd/messageFictif.java
@@ -0,0 +1,28 @@
+package fr.upjv.eijv_chat_androd;
+
+
+
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+
+
+
+
+
+
+public class messageFictif {
+    public static void main(String[] args) {
+
+        FirebaseAuth mAuth;
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();}
+}
+
+
