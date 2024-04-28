82e413f190a1b62986002374ad35479d8ddec92e
root <venceslasgbaguidi@yahoo.com>
2024-04-28 22:17:15 +0200

    Correction des erreurs

--- app/src/main/java/fr/upjv/eijv_chat_androd/MessageAdapter.java
+++ app/src/main/java/fr/upjv/eijv_chat_androd/MessageAdapter.java
@@ -1,3 +1,4 @@
+//MessageAdapter
 package fr.upjv.eijv_chat_androd;
 
 import android.view.LayoutInflater;
@@ -6,9 +7,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
-import fr.upjv.eijv_chat_androd.MessageViewHolder;
-import fr.upjv.eijv_chat_androd.messageFictif;
-import fr.upjv.eijv_chat_androd.R;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
     private final List<messageFictif> lesMessages;
@@ -18,20 +16,18 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
     }
 
     @NonNull
-    @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.messageligne, parent, false);
         return new MessageViewHolder(view);
     }
 
-    @Override
+
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
         messageFictif message = lesMessages.get(position);
         holder.bind(message);
     }
 
-    @Override
     public int getItemCount() {
         return lesMessages.size();
     }
