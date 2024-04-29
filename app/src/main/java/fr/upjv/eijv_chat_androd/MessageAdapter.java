b3fcec726b4e0c0c4af9df62f352b4c7cc427501
venceslas1 <venceslasgbaguidi@yahoo.com>
2024-04-29 17:41:38 +0200

    Correction des erreurs

--- app/src/main/java/fr/upjv/eijv_chat_androd/MessageAdapter.java
+++ app/src/main/java/fr/upjv/eijv_chat_androd/MessageAdapter.java
@@ -10,6 +10,49 @@ import java.util.List;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
     private final List<messageFictif> lesMessages;
+    private final String mySenderIdentifier = "Moi"; // Identifier de l'utilisateur actuel
+    private static final int VIEW_TYPE_ME = 1;
+    private static final int VIEW_TYPE_OTHER = 2;
+
+    public MessageAdapter(List<messageFictif> lesMessages) {
+        this.lesMessages = lesMessages;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        messageFictif message = lesMessages.get(position);
+        return message.getSender().equals(mySenderIdentifier) ? VIEW_TYPE_ME : VIEW_TYPE_OTHER;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view;
+        if (viewType == VIEW_TYPE_ME) {
+            view = inflater.inflate(R.layout.messageligne, parent, false);
+        } else {
+            view = inflater.inflate(R.layout.messageautre, parent, false);
+        }
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        messageFictif message = lesMessages.get(position);
+        holder.bind(message);
+    }
+
+    @Override
+    public int getItemCount() {
+        return lesMessages.size();
+    }
+}
+
+
+
+/*public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
+    private final List<messageFictif> lesMessages;
 
     public MessageAdapter(List<messageFictif> lesMessages) {
         this.lesMessages = lesMessages;
@@ -31,4 +74,4 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
     public int getItemCount() {
         return lesMessages.size();
     }
-}
+}*/
