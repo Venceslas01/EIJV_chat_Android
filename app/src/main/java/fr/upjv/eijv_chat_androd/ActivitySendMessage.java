0530b17af9f6bfbc6d462ec2b6026b9f9ae32692
js <jarcadias@gmail.com>
2024-04-29 18:19:19 +0200

    Commit envoi message

---
+++ app/src/main/java/fr/upjv/eijv_chat_androd/ActivitySendMessage.java
@@ -0,0 +1,60 @@
+package fr.upjv.eijv_chat_androd;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ActivitySendMessage {
+
+    private FirebaseFirestore maBase;
+    private EditText editTextMessage;
+
+    public ActivitySendMessage(Context context, EditText editTextMessage) {
+        maBase = FirebaseFirestore.getInstance();
+        this.editTextMessage = editTextMessage;
+    }
+
+    public void onClickMessageToSend(View view){
+
+        Map<String, Object> maMap = new HashMap<>();
+
+        String contenuMessage = editTextMessage.getText().toString().trim();
+
+        if (contenuMessage.isEmpty()){
+            return;
+        }
+
+        long timestamp = System.currentTimeMillis();
+        String uuid = generateUUID();
+
+        maMap.put("timestamp", timestamp);
+        maMap.put("contenu", contenuMessage);
+        maMap.put("uuid", uuid);
+
+        maBase.collection("CHAT").add(maMap)
+                .addOnSuccessListener(documentReference -> {
+                    editTextMessage.setText("");
+                    showToast("Envoyé");
+                    return;
+                });
+
+        String text = "Envoyé !";
+        int duree = Toast.LENGTH_SHORT;
+        Toast toastSendMessage = Toast.makeText(view.getContext(), text, duree);
+        toastSendMessage.show();
+    }
+
+    private String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+    private void showToast(String message) {
+        Toast.makeText(editTextMessage.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
--- app/src/main/java/fr/upjv/eijv_chat_androd/MainActivity.java
+++ app/src/main/java/fr/upjv/eijv_chat_androd/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,6 +16,7 @@ import fr.upjv.eijv_chat_androd.activityScroll;
 public class MainActivity extends AppCompatActivity {
 
     private Button bouttonLancerScrol;
+    private EditText editTextMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
             Intent monIntent = new Intent(this, activityScroll.class);
             startActivity(monIntent);
         });
+
+        editTextMessage = findViewById(R.id.id_messageToSend);
+
+        ActivitySendMessage sendMessage =
+                new ActivitySendMessage(this, editTextMessage);
+
+        Button buttonSendMessage = findViewById(R.id.id_envoyer_messsage);
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage.onClickMessageToSend(v);
+            }
 
     }
 
